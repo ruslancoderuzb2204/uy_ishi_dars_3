af348949ca3b30b1804c3642d8e9f57cb8402b20
ruslantilovov2 <ruslantilovov2@gmail.com>
2022-12-13 08:18:10 +0500

    change

---
+++ main.js
@@ -0,0 +1,10 @@
+let a = +prompt("Son kiriting!")
+if(a % 2 == 0){
+    console.log(a**=2);
+}
+else if(a % 2 == 1){
+    console.log(a**=3);
+}
+else{
+    console.log("Iltomos faqat raqam kiriting!");
+}
